6564009eb00e0dbac7562ec28485cb36fc5856e6
Shaikh Al Amin <alamin@tikweb.com>
2024-06-10 09:52:10 +0600

    job cache issue debugging

--- setup.sh
+++ setup.sh
@@ -4,17 +4,16 @@ echo "Backend env copying ..."
 cp news-aggregator-be/.env.example news-aggregator-be/.env
 
 echo "Stoping all container ..."
-docker-compose down --remove-orphans
-
+docker-compose down -v --remove-orphans
 echo "Frontend env copying ..."
 cp news-aggregator-fe/.env.example news-aggregator-fe/.env
 
 echo "New docker-compose build started ..."
 echo "Please wait for a while to build with no cache ...."
 echo "Run container with detach mode ...."
-# docker-compose up --build -d
-docker-compose build --no-cache
-docker-compose up -d
+docker-compose up --build -d
+# docker-compose build --no-cache
+# docker-compose up -d
 echo "Generating new backend app key ...."
 docker exec -it backend-container php artisan key:generate
 
@@ -31,12 +30,9 @@ echo "Generating demo seed data"
 docker exec -it backend-container php artisan db:seed
 
 docker exec -it backend-container php artisan key:generate
+echo "Please click http://localhost:7890 to visit the app"
+docker exec -it backend-container php artisan queue:work
 
-docker exec -it laravel-supervisor composer install
-docker exec -it laravel-supervisor composer dump-autoload --no-scripts
-
-docker exec -it backend-container chmod 775 -R /var/www/html/
-docker exec -it backend-container chown -R www-data:www-data /var/www/html/
 
 # Fetch news feed
 # docker exec -it backend-container php artisan newsfeed:fetch
@@ -44,4 +40,4 @@ docker exec -it backend-container chown -R www-data:www-data /var/www/html/
 # echo "Running lint on frontend container"
 # docker exec -it frontend-container npm i dompurify
 
-echo "Please click http://localhost:7890 to visit the app"
+
