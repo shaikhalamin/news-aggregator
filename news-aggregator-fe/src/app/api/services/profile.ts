d6dcaca51164668176e1117d48a31ed982c83af5
Shaikh Al Amin <alamin@tikweb.com>
2024-07-04 04:56:11 +0600

    source api call added from frontend

--- news-aggregator-fe/src/app/api/services/profile.ts
+++
@@ -1,9 +0,0 @@
-import { axiosPrivate } from '../lib/axios-private'
-
-export const getUserProfile = async (username) => {
-  return axiosPrivate.get(`/profiles/${username}`, {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-}
